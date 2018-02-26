20829b6b5a6fef2566c674d4c5ce24f4f52162b1
Ryan Manly <ryan.manly@gmail.com>
2018-02-26 11:02:11 -0600

    added TODOs

--- detach_nonmounted_dmg.py
+++ detach_nonmounted_dmg.py
@@ -5,3 +5,6 @@ process = subprocess.Popen(cmd, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
 out, _ = process.communicate()
 if 'content-hint' in out and not 'mount-point' in out:
     print 'bad'
+
+# TODO: Eric Holtam mentioned I can just detach the disk# instead of rebooting
+# TODO: account for multiple images in various states
