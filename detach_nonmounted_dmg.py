e2529332f7ee1ac9176207ae8d0145d5a834fe28
Ryan Manly <ryan.manly@gmail.com>
2018-02-26 14:48:59 -0600

    this might work

--- detach_nonmounted_dmg.py
+++ detach_nonmounted_dmg.py
@@ -5,10 +5,40 @@ process = subprocess.Popen(cmd, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
 out, _ = process.communicate()
 plist = plistlib.readPlistFromString(out)
 
-i = 0
-# TODO: account for multiple images in various states
-for image in plist['images']:
-    for entity in image['system-entities']:
-            print entity
+def is_mounted(image):
+    '''Return whether the image shows a mount point
+    (plistlib._InternalDict) -> bool
+    >>> is_mounted(image)
+    True
+    >>> is_mounted(image)
+    False
+    '''
+    if 'mount-point' in str(image):
+        return
 
-# TODO: Eric Holtam mentioned I can just detach the disk# instead of rebooting
+def get_dev_name(entity):
+    '''Return the base device name for a given entity
+    (list) -> string
+    
+    >>> get_disk(entity)
+    /dev/disk2
+
+    '''
+    return entity[0]['dev-entry']
+
+def detach(dev_name):
+    cmd = ['/usr/bin/hdiutil', 'detach', dev_name]
+    try:
+        subprocess.check_call(cmd)
+    except subprocess.CalledProcessError:
+        print 'Could not detach disk' + dev_name
+
+
+# TODO: need to check if nothing is mounted first
+for image in plist['images']:
+    if is_mounted(image):
+        continue
+    dev_name = get_dev_name(image['system-entities'])
+    # Toyed with the idea of logging…I don't care enough
+    # name = dev_name.split('/')[-1]
+    detach(dev_name)
